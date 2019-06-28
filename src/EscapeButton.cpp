00e62485920721f609a30d7ea33bfa0c76be1b99
ern0 <ern0@linkbroker.hu>
2019-06-28 18:16:10 +0200

    without print()

--- src/EscapeButton.cpp
+++ src/EscapeButton.cpp
@@ -2,26 +2,31 @@
 
 
 #define PIN_BUTTON 1
+#define KEY_ESC 41
 
 
 	void setup() {
-
-	  DigiKeyboard.update();
-	  pinMode(PIN_BUTTON,INPUT);
 
 	} // setup()
 
 
 	void loop() {
 
-		while ( digitalRead(PIN_BUTTON) == 0 );
+		while ( digitalRead(PIN_BUTTON) == 0 ) {
+			DigiKeyboard.delay(10);
+		}
 
-	  DigiKeyboard.print("\e");
-	  DigiKeyboard.update();
+		DigiKeyboard.update();
+		DigiKeyboard.sendKeyPress(KEY_ESC,0);
+		DigiKeyboard.delay(200);			
+		DigiKeyboard.sendKeyPress(0,0);   
 
-		while ( digitalRead(PIN_BUTTON) == 1 );
 
-		delay(500);
+		while ( digitalRead(PIN_BUTTON) == 1 ) {
+			DigiKeyboard.delay(10);			
+		}
+	  
+		DigiKeyboard.delay(100);
 
 	} // loop()
 
